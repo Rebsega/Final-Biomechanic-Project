3d625cbff6bc344c0f7c51e0a9bd3670ae44ae80
Rebsega <renan.barbieri.s@gmail.com>
2021-11-26 22:53:37 -0300

    Adding some codes (py notebook and .py code)

---
+++ FinalCode_Clara_Renan.py
@@ -0,0 +1,9 @@
+#Code for the Last project of the Biomechanic subject
+#Authors:
+#Clara Alves Coelho
+#Renan Barbieri Segamarchi
+#####################################################
+#This code is made for receiving a video, chosen by the authors or the user, then this one will have some options such as:
+#Cut the video in a specific part then generate images of this piece
+#Show the number of frames
+#Generate an video of images
